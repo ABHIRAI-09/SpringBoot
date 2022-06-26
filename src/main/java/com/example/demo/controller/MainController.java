87a7c5354611977343d197940ba8151fa4a87810
ABHIRAI-09 <abhishekrai0907@gmail.com>
2022-06-26 18:31:11 +0530

    changes

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,24 +16,23 @@ import com.example.demo.data.CustomerInfo;
 import com.example.demo.service.CustomerService;
 
 @RestController
-@Validated
 public class MainController {
 	
 	@Autowired
 	CustomerService customerService;
-	@ResponseBody
+	//@ResponseBody
 	@GetMapping("/")
 	public String start( ) {
 		
 		return "Abhishek rai";
 	}
-	@ResponseBody
+	//@ResponseBody
 	@GetMapping("/welcome")
 	public String welcome( ) {
 		
 		return "Welcome\n first page";
 	}
-	@ResponseBody
+	//@ResponseBody
 	@GetMapping("/customers/{customerId}")
 	public CustomerBean  getCustomers( @PathVariable String customerId) {
 		
@@ -42,7 +40,7 @@ public class MainController {
 	}
 	
 	
-	@ResponseBody
+	//@ResponseBody
 	@PostMapping("/customers")
 	public CustomerInfo  posCustomers( @RequestBody CustomerDetails customerDetails) {
 		CustomerBean customerBean= new CustomerBean();
