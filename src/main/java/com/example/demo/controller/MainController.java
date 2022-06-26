c1e23a1e7e4cf4b9e9e55783e966dd1f99713716
ABHIRAI-09 <abhishekrai0907@gmail.com>
2022-06-26 18:48:48 +0530

    Chnges

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,35 +22,35 @@ public class MainController {
 	
 	@Autowired
 	CustomerService customerService;
-	//@ResponseBody
+	@ResponseBody
 	@GetMapping("/")
-	public String start( ) {
+	public ResponseEntity<String> start( ) {
 		
-		return "Abhishek rai";
+		return new ResponseEntity<>("Hello World!", HttpStatus.OK);
 	}
-	//@ResponseBody
+	@ResponseBody
 	@GetMapping("/welcome")
-	public String welcome( ) {
+	public ResponseEntity<String> welcome( ) {
 		
-		return "Welcome\n first page";
+		return new ResponseEntity<>("Welcome\n first page", HttpStatus.OK);
 	}
-	//@ResponseBody
+	@ResponseBody
 	@GetMapping("/customers/{customerId}")
-	public CustomerBean  getCustomers( @PathVariable String customerId) {
+	public ResponseEntity<CustomerBean>  getCustomers( @PathVariable String customerId) {
 		
-		return customerService.service(customerId);
+		return new ResponseEntity<>(customerService.service(customerId), HttpStatus.OK);
 	}
 	
 	
-	//@ResponseBody
+	@ResponseBody
 	@PostMapping("/customers")
-	public CustomerInfo  posCustomers( @RequestBody CustomerDetails customerDetails) {
+	public ResponseEntity<CustomerInfo>  posCustomers( @RequestBody CustomerDetails customerDetails) {
 		CustomerBean customerBean= new CustomerBean();
 		customerBean.setId(customerDetails.getId());
 		customerBean.setName(customerDetails.getName());
 		customerBean.setCompanyName(customerDetails.getCompany());
 		System.out.println(customerBean.toString());
-		return customerService.postService(customerBean);
+		return new ResponseEntity<>(customerService.postService(customerBean),HttpStatus.OK);
 		//return customerService.service(customerBean);
 	}
 
